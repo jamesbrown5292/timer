7bd95eee5e1c9a34179411be9a43decfc6922a4b
James Brown <jamesbrown5292@gmail.com>
2020-08-25 01:11:24 +0000

    changed output to a beep

--- timer1.js
+++ timer1.js
@@ -3,7 +3,7 @@ let argsArray = process.argv.slice(2);
 for (let arg of argsArray) {
   if (arg > 0) {
     setTimeout(() => {
-      process.stdout.write('beep\n');
+      process.stdout.write('\x07');
     }, arg);
   }
 }
